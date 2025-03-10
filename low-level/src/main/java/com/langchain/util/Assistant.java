2dd620613d35dac0b945610583122241e87f6ac0
qiaoyansong <1012077930@qq.com>
2025-03-10 23:57:04 +0800

    first commit

---
+++ low-level/src/main/java/com/langchain/util/Assistant.java
@@ -0,0 +1,10 @@
+package com.langchain.util;
+
+/**
+ * @author ：Yunchenyan
+ * @date ：Created in 2025/3/10 23:56
+ * description：
+ */
+public interface Assistant {
+    String answer(String userQuery);
+}
---
+++ low-level/src/main/java/com/langchain/util/LangchainUtil.java
@@ -0,0 +1,34 @@
+package com.langchain.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Scanner;
+
+/**
+ * @author ：Yunchenyan
+ * @date ：Created in 2025/3/10 23:55
+ * description：
+ */
+public class LangchainUtil {
+
+    public static void startConversationWith(Assistant assistant) {
+        Logger log = LoggerFactory.getLogger(Assistant.class);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                log.info("==================================================");
+                log.info("User: ");
+                String userQuery = scanner.nextLine();
+                log.info("==================================================");
+
+                if ("exit".equalsIgnoreCase(userQuery)) {
+                    break;
+                }
+
+                String agentAnswer = assistant.answer(userQuery);
+                log.info("==================================================");
+                log.info("Assistant: " + agentAnswer);
+            }
+        }
+    }
+}
